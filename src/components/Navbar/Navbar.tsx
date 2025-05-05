a5acc864bab41f8436a7c53244d016f29ec4a0ce
Juan Daniel Martinez <juanda.martinezn@gmail.com>
2025-05-05 00:59:44 -0600

    feat: Add Avatar component to Navbar; update package dependencies

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 
 import NavigationMenu from '@/components/Navbar/NavigationMenu';
+import { Avatar, AvatarImage, AvatarFallback } from '@/components/ui/Avatar';
 
 export default function Navbar() {
   return (
     <div
       className={
-        'p-075 rounded-10 bg-neutral-0 fixed top-200 right-200 left-200 flex items-center justify-between border-1 border-neutral-200 shadow-[0_6px_10px_0_rgba(123,123,123,0.03)]'
+        'p-075 rounded-10 bg-neutral-0 fixed top-200 right-200 left-200 z-99 flex items-center justify-between border-1 border-neutral-200 shadow-[0_6px_10px_0_rgba(123,123,123,0.03)]'
       }
     >
+      <Avatar className={'rounded-10 h-[40px] w-[40px]'}>
+        <AvatarImage src="https://github.com/juandadev.png" />
+        <AvatarFallback>JM</AvatarFallback>
+      </Avatar>
       <div>
         <NavigationMenu />
       </div>
