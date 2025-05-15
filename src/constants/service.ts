8397dffd01aa2a44fcd0f5d4dbcb24587ee5e5c1
Juan Daniel Martinez <juanda.martinezn@gmail.com>
2025-05-14 21:59:35 -0600

    feat: Introduce Post model and API response structure for fetching published posts

---
+++ src/constants/service.ts
@@ -0,0 +1,41 @@
+export const API_ERRORS = {
+  NOT_FOUND: {
+    message: 'Resource not found',
+    status: 404,
+  },
+  UNAUTHORIZED: {
+    message: 'Unauthorized access',
+    status: 401,
+  },
+  FORBIDDEN: {
+    message: 'Forbidden access',
+    status: 403,
+  },
+  INTERNAL_SERVER_ERROR: {
+    message: 'Internal server error',
+    status: 500,
+  },
+};
+
+export const POST_SUCCESS = {
+  CREATED: {
+    message: 'Post created successfully',
+    status: 201,
+  },
+  UPDATED: {
+    message: 'Post updated successfully',
+    status: 200,
+  },
+  DELETED: {
+    message: 'Post deleted successfully',
+    status: 200,
+  },
+  FETCHED_MANY: {
+    message: 'Posts fetched successfully',
+    status: 200,
+  },
+  FETCHED_ONE: {
+    message: 'Post fetched successfully',
+    status: 200,
+  },
+};
---
+++ src/types/post.ts
@@ -0,0 +1,23 @@
+import { User } from '@/types/user';
+
+export interface Post {
+  id: number;
+  title: string;
+  slug: string;
+  publishedAt: Date;
+  coverImage: string | null;
+  originalPostUrl: string | null;
+  tags: string[];
+  description: string;
+  content: string;
+  status: PostStatus;
+  author: Pick<User, 'name' | 'profilePicture'>;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export type PostStatus = 'DRAFT' | 'PUBLISHED' | 'ARCHIVED';
+
+export interface GetPostsResponse {
+  posts: Post[];
+}
