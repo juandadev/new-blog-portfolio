7b23be163c01be96cac5cd67baec9d3573c8d217
Juan Daniel Martinez <juanda.martinezn@gmail.com>
2025-05-15 12:42:53 -0600

    feat: Enhance MarkdownRenderer with custom components for images, links, and lists

--- src/components/MarkdownRenderer/MarkdownRenderer.tsx
+++ src/components/MarkdownRenderer/MarkdownRenderer.tsx
@@ -2,9 +2,28 @@ import React from 'react';
 import ReactMarkdown from 'react-markdown';
 import { Heading } from '@/components/ui/Heading';
 import { Typography } from '@/components/Typography/Typography';
+import { Separator } from '@/components/ui/Separator';
+import Link from '@/components/ui/Link';
+import { AspectRatio } from '@/components/ui/AspectRatio';
+import Image from 'next/image';
 
 interface MarkdownRendererProps {
   content: string;
+}
+
+function MarkdownImage({ alt, src }: { src: string; alt: string }) {
+  return (
+    <AspectRatio ratio={12 / 9}>
+      <Image
+        className={'rounded-12'}
+        alt={alt!}
+        src={src as string}
+        fill
+        sizes={'(max-width: 639px) 100vw, 576px'}
+        objectFit={'cover'}
+      />
+    </AspectRatio>
+  );
 }
 
 export default function MarkdownRenderer({ content }: MarkdownRendererProps) {
@@ -36,9 +55,55 @@ export default function MarkdownRenderer({ content }: MarkdownRendererProps) {
             {children}
           </Heading>
         ),
-        p: ({ children, ...props }) => (
-          <Typography {...props}>{children}</Typography>
+        p: ({ children }) => {
+          if (
+            React.Children.count(children) === 1 &&
+            // @ts-expect-error I don't know how to fix the type
+            children?.props?.node?.tagName === 'img'
+          )
+            return <>{children}</>;
+
+          return <Typography>{children}</Typography>;
+        },
+        hr: () => <Separator className={'my-150'} />,
+        ul: ({ children, ...props }) => (
+          <ul className={'list-disc'} {...props}>
+            {children}
+          </ul>
         ),
+        ol: ({ children, ...props }) => (
+          <ol className={'list-decimal'} {...props}>
+            {children}
+          </ol>
+        ),
+        li: ({ children, ...props }) => (
+          <li className={'ml-300'} {...props}>
+            <Typography>{children}</Typography>
+          </li>
+        ),
+        blockquote: ({ children, ...props }) => (
+          <blockquote
+            className={
+              'border-l-4 border-neutral-400 pl-150 dark:border-neutral-600'
+            }
+            {...props}
+          >
+            <Typography>{children}</Typography>
+          </blockquote>
+        ),
+        a: ({ children, href, ...props }) => (
+          <Link
+            href={href!}
+            className={
+              'relative inline-block w-fit after:absolute after:bottom-0 after:left-0 after:block after:h-[3px] after:w-full after:bg-blue-500 hover:text-current/70'
+            }
+            {...props}
+            passHref
+          >
+            {children}
+          </Link>
+        ),
+        img: ({ alt, src }) => <MarkdownImage alt={alt!} src={src as string} />,
       }}
     >
       {content}
