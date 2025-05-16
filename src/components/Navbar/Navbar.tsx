af09801bc82a390e478495e09786e81ca61e7365
Juan Daniel Martinez <juanda.martinezn@gmail.com>
2025-05-15 19:34:26 -0600

    feat: Add background overlay to Navbar for improved visibility and aesthetics

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -7,22 +7,25 @@ import Link from '@/components/ui/Link';
 
 export default function Navbar() {
   return (
-    <header
-      className={
-        'p-075 rounded-10 bg-card border-border fixed top-200 right-200 left-200 z-40 flex max-w-[640px] items-center justify-between border-1 shadow-[0_6px_10px_0_rgba(123,123,123,0.03)] sm:right-auto sm:left-auto sm:w-full'
-      }
-    >
-      <Avatar className={'rounded-10 h-[40px] w-[40px]'} asChild>
-        <Link href={'/'}>
-          <AvatarImage src="https://github.com/juandadev.png" />
-          <AvatarFallback>JM</AvatarFallback>
-        </Link>
-      </Avatar>
+    <>
+      <div className={'bg-background fixed top-0 left-0 z-[9] h-4 w-full'} />
+      <header
+        className={
+          'p-075 rounded-10 bg-card border-border fixed top-200 right-200 left-200 z-40 flex max-w-[640px] items-center justify-between border-1 shadow-[0_6px_10px_0_rgba(123,123,123,0.03)] sm:right-auto sm:left-auto sm:w-full'
+        }
+      >
+        <Avatar className={'rounded-10 h-[40px] w-[40px]'} asChild>
+          <Link href={'/'}>
+            <AvatarImage src="https://github.com/juandadev.png" />
+            <AvatarFallback>JM</AvatarFallback>
+          </Link>
+        </Avatar>
 
-      <div className={'gap-075 flex md:gap-250'}>
-        <NavbarMenu />
-        <ControlPanelTrigger />
-      </div>
-    </header>
+        <div className={'gap-075 flex md:gap-250'}>
+          <NavbarMenu />
+          <ControlPanelTrigger />
+        </div>
+      </header>
+    </>
   );
 }
