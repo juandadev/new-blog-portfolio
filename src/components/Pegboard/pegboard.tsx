8dc38b5c9c9f4773ffcdb3d4b15708b61de912ff
Juan Daniel Martinez <juanda.martinezn@gmail.com>
2026-03-07 00:22:02 -0600

    feat: improving pegboard component and texture/pattern arrangement

--- src/components/Pegboard/pegboard.tsx
+++ src/components/Pegboard/pegboard.tsx
@@ -3,10 +3,8 @@ import Image from 'next/image';
 
 export default function Pegboard() {
   return (
-    <div className="wood-pegboard-shading absolute inset-0 m-auto flex h-[calc(round(down,100%-40px,59px)+40px)] w-[calc(round(down,100%-49px,62px)+49px)] overflow-hidden rounded-2xl bg-[#C4A47C] py-5 pr-[17px] pl-8">
-      <div className="absolute inset-0 overflow-hidden rounded-2xl bg-[url('/textures/fiberboard_pattern_black.png')] bg-[size:571px_400px] bg-repeat opacity-20" />
-      <div className="h-full w-full bg-[url('/textures/wood_skadis_shades.png')] bg-[size:62px_59px] bg-[position:-23px_-15px] bg-repeat" />
-
+    <div className="bg-skadis-wood absolute inset-0 isolate m-auto flex h-[calc(round(down,100%-40px,59px)+40px)] w-[calc(round(down,100%-49px,62px)+49px)] overflow-hidden rounded-2xl bg-[#C4A47C] py-5 pr-[17px] pl-8">
+      <p className="sr-only">Pegboard</p>
       {/* Screws */}
       <Image
         src="/pegboard/screw.png"
