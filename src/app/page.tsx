9f0d8d84c5dc17e6eeffe3e490b26d34260f3304
Juan Daniel Martinez <juanda.martinezn@gmail.com>
2026-03-06 21:00:59 -0600

    feat: pegboard screws

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Pegboard from '@/components/Pegboard/pegboard';
+import Polaroid from '@/components/Polaroid/polaroid';
 
 export default function Home() {
   return (
-    <div className="relative h-full w-full">
+    <div className="relative h-full w-full py-16">
+      <Polaroid />
       <Pegboard />
     </div>
   );
