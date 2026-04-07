329f3b656f1f48e5bff70692ab16fa23cc0add84
Juan Daniel Martinez <juanda.martinezn@gmail.com>
2026-04-07 13:41:18 -0600

    feat: aaaaaaah

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
   return (
     <div className="relative mx-auto h-full w-full max-w-[1440px] py-16">
       <Pegboard />
-      <div className="relative grid grid-cols-1 grid-rows-[repeat(3,auto)_minmax(300px,auto)_repeat(6,auto)] place-items-stretch gap-6 md:grid-cols-5 md:grid-rows-[auto_minmax(300px,auto)_repeat(2,auto)] xl:grid-rows-3">
+      <div className="relative grid grid-cols-1 grid-rows-[repeat(3,auto)_minmax(300px,auto)_repeat(6,auto)] place-items-stretch gap-6 md:grid-cols-5 md:grid-rows-[auto_minmax(300px,auto)_repeat(2,auto)] xl:grid-rows-[repeat(3,auto)]">
         <Polaroid
           src="/juan.webp"
           className="z-1 col-span-1 w-50 md:col-span-2 lg:col-span-1"
@@ -32,7 +32,7 @@ export default function Home() {
         <LatestPosts containerClassName="md:col-span-2 lg:col-span-3 xl:col-span-2" />
         <ToolsIUse containerClassName="md:col-span-5 xl:col-span-3" />
         <SocialCards containerClassName="md:col-span-2 lg:col-span-1" />
-        <GameNCoffee className="md:col-span-3 lg:col-span-2" />
+        <GameNCoffee className="self-end md:col-span-3 lg:col-span-2" />
         <LegoPieces containerClassName="lg:col-span-1 md:col-span-3" />
         <CurrentReading
           containerClassName="col-span-1 md:col-span-2 lg:col-span-1"
--- src/components/views/landing/game-n-coffee.tsx
+++ src/components/views/landing/game-n-coffee.tsx
@@ -32,57 +32,59 @@ const currentYear = new Date().getFullYear();
 
 export default function GameNCoffee({ className }: GameNCoffeeProps) {
   return (
-    <div className={cn('relative h-70', className)}>
-      <HoverCard openDelay={50} closeDelay={50}>
-        <HoverCardTrigger asChild>
-          <div
-            className={cn(
-              'absolute right-[calc(50%-50px)] bottom-1 aspect-[13.5/17] w-47.5 xl:right-1/2 xl:bottom-2',
-              'after:absolute after:inset-0 after:z-2'
-            )}
-          >
-            <GameCover variant="ps5" containerClassName="select-none" />
-          </div>
-        </HoverCardTrigger>
-        <HoverCardContent side="top" className="flex flex-col text-center">
-          <span className="font-script text-2xl font-medium">
-            Currently playing:
-          </span>
-          <span className="text-muted-foreground text-lg font-semibold">
-            Crimson Desert, PS5
-          </span>
-        </HoverCardContent>
-      </HoverCard>
-      <HoverCard openDelay={50} closeDelay={50}>
-        <HoverCardTrigger asChild>
-          <div
-            className={cn(
-              'absolute bottom-3 left-[calc(50%+5px)] xl:bottom-4 xl:left-[calc(50%+20px)]',
-              'after:absolute after:inset-0 after:z-2'
-            )}
-          >
-            <Image
-              src="/mugs/dev_mug.png"
-              alt="Mug"
-              width={591}
-              height={435}
-              unoptimized
+    <div className={className}>
+      <div className="relative h-70">
+        <HoverCard openDelay={50} closeDelay={50}>
+          <HoverCardTrigger asChild>
+            <div
               className={cn(
-                'aspect-[591/435] w-37.5 drop-shadow-md/25 select-none'
+                'absolute right-[calc(50%-50px)] bottom-1 aspect-[13.5/17] w-47.5 xl:right-1/2 xl:bottom-2',
+                'after:absolute after:inset-0 after:z-2'
               )}
-            />
-          </div>
-        </HoverCardTrigger>
-        <HoverCardContent side="top" className="flex flex-col text-center">
-          <span className="font-script text-2xl font-medium">
-            125 Cups of coffee in {currentYear}
-          </span>
-          <span className="text-muted-foreground font-semibold">
-            (and it&apos;s just {months[currentMonth]})
-          </span>
-        </HoverCardContent>
-      </HoverCard>
-      <Shelf />
+            >
+              <GameCover variant="ps5" containerClassName="select-none" />
+            </div>
+          </HoverCardTrigger>
+          <HoverCardContent side="top" className="flex flex-col text-center">
+            <span className="font-script text-2xl font-medium">
+              Currently playing:
+            </span>
+            <span className="text-muted-foreground text-lg font-semibold">
+              Crimson Desert, PS5
+            </span>
+          </HoverCardContent>
+        </HoverCard>
+        <HoverCard openDelay={50} closeDelay={50}>
+          <HoverCardTrigger asChild>
+            <div
+              className={cn(
+                'absolute bottom-3 left-[calc(50%+5px)] xl:bottom-4 xl:left-[calc(50%+20px)]',
+                'after:absolute after:inset-0 after:z-2'
+              )}
+            >
+              <Image
+                src="/mugs/dev_mug.png"
+                alt="Mug"
+                width={591}
+                height={435}
+                unoptimized
+                className={cn(
+                  'aspect-[591/435] w-37.5 drop-shadow-md/25 select-none'
+                )}
+              />
+            </div>
+          </HoverCardTrigger>
+          <HoverCardContent side="top" className="flex flex-col text-center">
+            <span className="font-script text-2xl font-medium">
+              125 Cups of coffee in {currentYear}
+            </span>
+            <span className="text-muted-foreground font-semibold">
+              (and it&apos;s just {months[currentMonth]})
+            </span>
+          </HoverCardContent>
+        </HoverCard>
+        <Shelf />
+      </div>
     </div>
   );
 }
