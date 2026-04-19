a079eb1196a50d066a91523f02bda77fabf95116
Juan Daniel Martinez <juanda.martinezn@gmail.com>
2026-04-18 22:31:01 -0600

    feat: randomly load mugs

--- src/components/views/landing/CoffeeCups.tsx
+++ src/components/views/landing/CoffeeCups.tsx
@@ -10,7 +10,12 @@ interface CoffeeCupsProps {
 
 const currentYear = new Date().getFullYear();
 
+const MUG_IMAGES = ['/mugs/dev_mug.webp', '/mugs/starbucks_mug.webp'];
+
 export default function CoffeeCups({ className }: CoffeeCupsProps) {
+  // eslint-disable-next-line
+  const mugSrc = MUG_IMAGES[Math.floor(Math.random() * MUG_IMAGES.length)];
+
   return (
     <div
       className={cn(
@@ -19,7 +24,7 @@ export default function CoffeeCups({ className }: CoffeeCupsProps) {
       )}
     >
       <Image
-        src="/mugs/dev.webp"
+        src={mugSrc}
         alt="Mug"
         width={399}
         height={400}
--- src/constants/drawings.ts
+++ src/constants/drawings.ts
@@ -1,5 +1,6 @@
 export const DRAWINGS = [
   { src: '/legos/vader.webp', alt: 'Lego Darth Vader drawing' },
-  { src: '/mugs/dev.webp', alt: 'Dev mug drawing' },
+  { src: '/mugs/dev_mug.webp', alt: 'Dev mug drawing' },
+  { src: '/mugs/starbucks_mug.webp', alt: 'Starbucks mug drawing' },
   { src: '/games/crimson_desert.webp', alt: 'Crimson Desert drawing' },
 ] as const;
