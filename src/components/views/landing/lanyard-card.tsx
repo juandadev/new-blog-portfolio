ec4d35fd4f0fff74aae01d9e4e3b98e14dcd0019
Juan Daniel Martinez <juanda.martinezn@gmail.com>
2026-07-16 22:27:04 -0600

    feat: update event

--- src/components/views/landing/lanyard-card.tsx
+++ src/components/views/landing/lanyard-card.tsx
@@ -22,7 +22,7 @@ export default function LanyardCard() {
       />
       <StickerLabel position="z-2 bottom-0 left-0">
         <Link
-          href="https://luma.com/eravc5ey"
+          href="https://www.cursorguadalajara.com/"
           target="_blank"
           rel="noopener noreferrer"
         >
